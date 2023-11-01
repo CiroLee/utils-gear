8c7d2560a30204c275f4761be126bd0bb3a26188
CiroLee <CiroMail@126.com>
2023-11-01 09:28:25 +0000

    Deploying to gh-pages from @ CiroLee/utils-gear@5e8f157212f36bee3ec259720b6d72ddac1f601a 🚀

---
+++ assets/api_browser.md.0384e716.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as a,c as p,Q as n}from"./chunks/framework.9ddf7cd9.js";const h=JSON.parse('{"title":"browser","description":"","frontmatter":{},"headers":[],"relativePath":"api/browser.md","filePath":"api/browser.md"}'),l={name:"api/browser.md"},o=n("",25),e=[o];function t(r,c,y,i,E,d){return a(),p("div",null,e)}const F=s(l,[["render",t]]);export{h as __pageData,F as default};
