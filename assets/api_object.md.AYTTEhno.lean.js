e63dbd2488702c2f0a7e5cae6614c409f0b13328
CiroLee <CiroMail@126.com>
2024-01-10 02:16:51 +0000

    Deploying to gh-pages from @ CiroLee/utils-gear@e9eaa5ee455570e0229a69784a7de012f53f7ad8 🚀

--- assets/api_object.md.AYTTEhno.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o as i,c as a,R as h}from"./chunks/framework.duMqqdHG.js";const c=JSON.parse('{"title":"object","description":"","frontmatter":{},"headers":[],"relativePath":"api/object.md","filePath":"api/object.md"}'),t={name:"api/object.md"},n=h("",36),p=[n];function k(l,e,E,r,d,g){return i(),a("div",null,p)}const F=s(t,[["render",k]]);export{c as __pageData,F as default};
